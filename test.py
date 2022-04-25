fc45eb4a6cd8ac3c027df3657d4b9d606a25e8aa
BUI DINH LOC <19511206@sv.ut.edu.vn>
2022-04-25 22:28:00 +0700

    final

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import vietnameseaccent as vn
 import vnchar
 import random
 
-sentence = "Xin chào"
+sentence = "Xin chào thầy và các bạn"
 sentence = vn.preprocess(sentence)          # Tiền xử lí
 _sentence = vn.remove_vn_accent(sentence)   # Bỏ dấu câu
 
 ekey = random.randrange(1, 95) # Cho random một key bất kì từ 1 đến 94 để thực hiện Ascii shift cipher
-print ("Key " + str(ekey))
+# print ("Key " + str(ekey))
 # ekey = 94
 _sentence = asciishift.encode(ekey, _sentence) 
 # print (_sentence)
@@ -21,8 +21,8 @@ index = 0
 length = len(_sentence)
 for result in results:
     index += 1
-    point = vnchar.check(result.lower()) # Chấm điểm dựa trên nhóm ký tự phổ biến của Tiếng Việt
-    if(point > length/5): # Lựa chọn mức độ điểm tương đối, nếu điểm càng cao thì số đề xuất sẽ ít
+    point = vnchar.checkSentence(result.lower()) # Chấm điểm dựa trên nhóm ký tự phổ biến của Tiếng Việt
+    if(point > length/4): # Lựa chọn mức độ điểm tương đối, nếu điểm càng cao thì số đề xuất sẽ ít
         print(str(index) + "-> " + result + " ->" + str(point) + "p") # In kết quả
 
 pos = input('Vị trí đề xuất kết quả: ') # Chọn kết quả đã decrypt để đề xuất sang Tiếng Việt
--- vnchar.py
+++ vnchar.py
@@ -9,7 +9,7 @@ vnchar = [
     'h', ['a', 'e', 'i', 'o', 'u', 'y'                     ],
     'i', ['a', 'c', 'e', 'm', 'n', 'p', 't', 'u'           ],
     'j', [],
-    'k', ['h'                                              ],
+    'k', ['h', 'e'                                         ],
     'l', ['a', 'e', 'i', 'o', 'u', 'y'                     ],
     'm', ['a', 'e', 'i', 'o', 'u', 'y'                     ],
     'n', ['a', 'e', 'g', 'h', 'i', 'o', 'u'                ],
@@ -18,7 +18,7 @@ vnchar = [
     'q', ['u'                                              ],
     'r', ['a', 'e', 'i', 'o', 'u'                          ],
     's', ['a', 'e', 'i', 'o', 'u', 'y'                     ],
-    't', ['a', 'e', 'h', 'i', 'o', 'u', 'y'                ],
+    't', ['a', 'e', 'h', 'i', 'o', 'r', 'u', 'y'           ],
     'u', ['a', 'c', 'e', 'i', 'm', 'n', 'o', 'p', 't', 'y' ],
     'v', ['a', 'e', 'i', 'o', 'u'                          ],
     'w', [],
@@ -27,18 +27,103 @@ vnchar = [
     'z', [],
 ]
 
-# text = "nananananananana"
-def check(text):
-    word = []
+text = "nghieng"
+def checkLetter(text):
+    letters = []
     for letter in text:
-        word.append(letter)
+        letters.append(letter)
 
     point = 0
     for i in range(0, len(text) - 1):
-        if(word[i] in vnchar):
-            id = vnchar.index(word[i])
-            if(word[i + 1] in vnchar[id + 1]):
+        if(letters[i] in vnchar):
+            id = vnchar.index(letters[i])
+            if(letters[i + 1] in vnchar[id + 1]):
                 point += 1
     return point
 
 # print(check(text))
+
+# Bảng phụ âm đầu
+consonant = [
+    # Đơn
+    [   'b', 'c', 'd', 'g', 'h',
+        'k', 'l', 'm', 'n', 'r',
+        's', 't', 'v', 'x'              ],
+    # Ghép đôi
+    [   'ch', 'gh', 'gi', 'kh', 'ng',
+        'nh', 'ph', 'qu', 'th', 'tr'    ],
+    # Ghép ba
+    [   'ngh'                           ]
+]
+
+# Bảng vần
+syllable = [
+    [   'a', 'e', 'i', 'o', 'u'             ],
+    [   
+        'ac', 'ai', 'am', 'an', 'ao',
+        'ap', 'at', 'au', 'ay', 'em',
+        'en', 'eo', 'ep', 'et', 'eu',
+        'ia', 'im', 'in', 'ip', 'it',
+        'iu', 'oa', 'oc', 'oe', 'oi',
+        'om', 'on', 'op', 'ot', 'ua',
+        'uc', 'ue', 'ui', 'um', 'un',
+        'uo', 'up', 'ut', 'uu', 'uy',       ],
+    [   
+        'ach', 'ang', 'anh', 'eng', 'ech',
+        'enh', 'ich', 'inh', 'ong', 'ung',
+        'iec', 'oai', 'iem', 'ien', 'iep',
+        'iet', 'ieu', 'oan', 'oat', 'oay',
+        'uan', 'uat', 'uoc', 'uoi', 'uom',
+        'oun', 'uot', 'uya', 'uyt', 'uop',
+        'uou', 'yen', 'yem', 'yeu'          ],
+    [   
+        'oach', 'ieng', 'oang', 'oanh', 
+        'oung', 'uych', 'uynh', 'uyen',
+        'uyet', 'oong'                      ],
+]
+
+def getCons(word):
+    cons = ''
+    for letter in word:
+        if(letter in consonant[0]):
+            cons += letter
+        else: 
+            break
+    return cons
+
+def getSyl(word):
+    syl = ''
+    isAdded = False
+    isSyllable = False
+    for letter in word:
+        isAdded = False
+        if(letter in syllable[0]):
+            syl += letter
+            isAdded = True
+            isSyllable = True
+        elif(isSyllable == True and isAdded == False):
+            syl += letter
+    return syl
+
+# text = "!@#$%^&*()an"
+def checkWord(word):
+    point = 0
+    cons = getCons(word)
+    syl = getSyl(word)
+
+    if(len(cons) < 5):
+        if(cons in consonant[len(cons) - 1]):
+            point += 1
+
+    if(len(syl) < 5):
+        if(syl in syllable[len(syl) - 1]):
+            point += 1
+    return point
+
+def checkSentence(sentence):
+    point = 0
+    words = sentence.split(' ')
+    point += checkLetter(sentence)
+    for word in words:
+        point += checkWord(word)
+    return point
