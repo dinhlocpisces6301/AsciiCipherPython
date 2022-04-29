c989c7e51994aef2593c07d1128491dcf8d6c13a
BUI DINH LOC <19511206@sv.ut.edu.vn>
2022-04-29 15:43:33 +0700

    Sort

--- asciishift.py
+++ asciishift.py
@@ -1,3 +1,5 @@
+import vnchar
+
 def encode(key, text):
     result = ""
     for letter in text:
@@ -15,9 +17,11 @@ def decode(key, text):
 #Trường hợp không biết trước Key (Vét cạn)
 def decode(text):
     results = []
-    for i in range(0, 256):
+    for i in range(1, 255):
         # dkey = 95 - (i % 95)  #From 32 -> 127
         dkey = 256 - (i % 256)  #From 0 -> 255
-        result = encode(dkey, text)
+        ptext = encode(dkey, text)
+        point = vnchar.checkSentence(ptext.lower())
+        result = [i, ptext, point]
         results.append(result)
     return results
--- interface.py
+++ interface.py
@@ -16,21 +16,44 @@ def btn_start():
     sentence = vn.preprocess(sentence)          # Tiền xử lí
     _sentence = vn.remove_vn_accent(sentence)   # Bỏ dấu câu
     ekey = random.randrange(1, 255) # Cho random một key bất kì từ 1 đến 255 để thực hiện Ascii shift cipher
+    key.set(ekey)
     _sentence = asciishift.encode(ekey, _sentence)
     encodeRes.set(_sentence)
+    cb.set(0)
+    isChecked()
 
 def btn_decode(_sentence):
     tree.delete(*tree.get_children())
     results = asciishift.decode(_sentence)
-    length = len(_sentence)
-    index = 0
-    for result in results:
-        index += 1
-        # Chấm điểm dựa trên nhóm ký tự phổ biến của Tiếng Việt
-        point = vnchar.checkSentence(result.lower())
-        if(point > length/3):  # Lựa chọn mức độ điểm tương đối, nếu điểm càng cao thì số đề xuất sẽ ít
+    sortedResults = sorted(results, key=lambda x: x[2], reverse=True)
+
+    for result in sortedResults:
+        if(result[2] > 0):  # Lựa chọn mức độ điểm tương đối, nếu điểm càng cao thì số đề xuất sẽ ít
             tree.insert('', 'end', values=(
-                str(index), str(result), str(point)))
+                str(result[0]), str(result[1]), str(result[2])))
+
+def vn_sugguest(event):
+    for it in tree.selection():
+        item = tree.item(it)
+        words = item['values'][1]
+        try:
+            words = words.split()
+            k = 5
+            vn_results = vn.beam_search(words, k)
+            tree1.delete(*tree1.get_children())
+
+            for i in range(0, k):
+                tempwrd = (' '.join(vn_results[i][0]))
+                tree1.insert('', 'end', values=(
+                    str(i+1), str(tempwrd)))
+        except:
+            tree1.insert('', 'end', values=(''))
+
+def isChecked():
+    if cb.get() == 1:
+        tkey.configure(text=key.get())
+    if cb.get() == 0:
+        tkey.configure(text='***')
 
 win = Tk()
 win.geometry("600x700")
@@ -41,6 +64,8 @@ style.configure("Treeview.Heading", font=("Segue UI", 14))
 style.configure("Treeview", font=("Segue UI", 12))
 
 encodeRes = StringVar()
+key = IntVar()
+results = []
 
 win.title("ASCII Shift Cipher")
 
@@ -48,7 +73,7 @@ lbTitle = Label(win, text="ASCII Shift Cipher", font=("Segue UI", 24, BOLD))
 lbTitle.grid(row=0, column=0, columnspan=2)
 # section1
 lb0 = Label(win, text="Enter a sentence:", font=("Segue UI", 14))
-lb0.grid(row=1, column=0, sticky=W, padx=21)
+lb0.grid(row=1, column=0, sticky=W, padx=25)
 
 tbword = Entry(win, width=48, font=('Segue UI', 14), justify=CENTER)
 tbword.grid(row=2, column=0, columnspan=2, pady=10)
@@ -58,10 +83,19 @@ btn0.grid(row=3, column=0, columnspan=2, pady=10)
 
 # encode section
 lb2 = Label(win, text="Encrypt result", font=("Segue UI", 14))
-lb2.grid(row=4, column=0, columnspan=2)
+lb2.grid(row=4, column=0, sticky=W, padx=25)
+
+lbkey = Label(win, text="Key:", font=("Segue UI", 14))
+lbkey.grid(row=4, column=0, sticky=E)
+tkey = Label(win,text='***', width=4, font=('Segue UI', 14), justify=CENTER)
+tkey.grid(row=4, column=1, sticky=W)
+
+cb = IntVar(value=0)
+ckb = Checkbutton(win, text="Show", variable=cb, onvalue=1, offvalue=0, command=isChecked)
+ckb.grid(row=4, column=1, sticky=E, padx=20)
+
 lb2_1 = Entry(win, width=48, font=('Segue UI', 14), justify=CENTER, textvariable=encodeRes)
 lb2_1.grid(row=5, column=0, columnspan=2)
-
 
 # decode section
 btn3 = Button(win, text='Decrypt', height=2, width=24,
@@ -69,7 +103,7 @@ btn3 = Button(win, text='Decrypt', height=2, width=24,
 btn3.grid(row=6, column=0, columnspan=2, pady=10)
 
 # decode section - List View
-lb4 = Label(win, text="Decrypt result(s):", font=("Segue UI", 14))
+lb4 = Label(win, text="Decrypt result(s)", font=("Segue UI", 14))
 lb4.grid(row=7, column=0, sticky=SW, padx = 20, pady=10)
 
 tree = ttk.Treeview(win, column=(1, 2, 3), show='headings', height=5)
@@ -81,23 +115,6 @@ tree.heading(2, text="Value", anchor=CENTER)
 tree.column(2, anchor=CENTER, minwidth=0, width=400)
 tree.heading(3, text="Point", anchor=CENTER)
 tree.column(3, anchor=CENTER, minwidth=0, width=80)
-
-def vn_sugguest(event):
-    for it in tree.selection():
-        item = tree.item(it)
-        words = item['values'][1]
-        try:
-            words = words.split()
-            k = 3
-            vn_results = vn.beam_search(words, k)
-            tree1.delete(*tree1.get_children())
-
-            for i in range(0, k):
-                tempwrd = (' '.join(vn_results[i][0]))
-                tree1.insert('', 'end', values=(
-                    str(i+1), str(tempwrd)))
-        except:
-            tree1.insert('', 'end', values=(''))
 
 tree.bind('<ButtonRelease-1>', vn_sugguest)
 
